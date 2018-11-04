dcec7ae909ba58db1805066916e0c30dd47a6ba1
IvaTsu <ivan.tsud@gmail.com>
2018-11-04 17:31:53 +0200

    Implement stateless function

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
+import Theme from './styles/Theme';
 import Header from './components/Header/Header';
+import AddPicturesButton from './components/AddPictures/AddPicturesButton';
+import RadioGroup from './components/RadioGroup';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Header />
-      </View>
-    );
-  }
-}
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <View>
+      <Header />
+      <AddPicturesButton />
+      <RadioGroup />
+    </View>
+  </ThemeProvider>
+);
